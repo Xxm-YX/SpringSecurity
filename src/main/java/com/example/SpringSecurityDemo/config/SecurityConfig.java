fac49181a199624f0d6f0250d7f328ed12f9882a
zyx <zxxm.07.13gmail.com>
2021-06-03 16:20:45 +0800

    20210603

--- src/main/java/com/example/SpringSecurityDemo/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityDemo/config/SecurityConfig.java
@@ -1,18 +1,27 @@
 package com.example.SpringSecurityDemo.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 import java.io.PrintWriter;
 
 @Configuration
@@ -27,6 +36,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        return delegatingPasswordEncoder;
 //    }
 
+    @Autowired
+    public DataSource dataSource;
+
+    @Bean
+    public JdbcTokenRepositoryImpl jdbcTokenRepository(){
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
+
     @Bean
     //不加密
     PasswordEncoder passwordEncoder() {
@@ -37,10 +56,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication() //开启在内存中定义用户
                 .withUser("zyx")//用户名
-                .password("456")//密码
-                .roles("admin"); //角色
+                .password("123")//密码
+                .roles("admin") //角色
+                .and()
+                .withUser("xxm")
+                .password("123")
+                .roles("user");
 
         //配置用户用and进行连接。
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("zyx").password("123").roles("admin").build());
+        manager.createUser(User.withUsername("xxm").password("123").roles("user").build());
+        return manager;
     }
 
     @Override
@@ -53,15 +84,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         );
     }
 
+    /**
+     * 角色继承
+     * @return
+     */
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy("ROLE_admin > ROLE_user");
+        return hierarchy;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
+        http.authorizeRequests()//基于URL的限制访问开启
+                .antMatchers("/rememberme/**").rememberMe()//需要rememberMe才能进行访问
+                .antMatchers("/admin/**").fullyAuthenticated()//  fullAuthenticated 不包含rememberMe的验证，authenticated包含rememberMe的验证
+//                .antMatchers("/admin/**").hasRole("admin")//配置访问路径，那些
+//                .antMatchers("/user/**").hasRole("user")
+                .anyRequest().authenticated()////映射所有请求  并进行验证
                 .and()//表示结束当前标签，上下文回到HttpSecurity，开启新一轮的配置。
                 .formLogin()
-                .loginPage("/login.html")
-                .loginProcessingUrl("/doLogin")
+//                .loginPage("/login.html")
+//                .loginProcessingUrl("/doLogin")
                 .permitAll()//表示登录相关的页面/接口不要被拦截。
                 .usernameParameter("name")
                 .passwordParameter("passwd")
@@ -90,21 +135,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()//GET请求
                 .logoutUrl("/logout")//修改默认注销URL
 //                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","POST")) //修改注销URL，还可以修改请求方式
-                .logoutSuccessUrl("/index")//登录成功要跳转的页面
+                .logoutSuccessHandler((req,resp,authentication) -> { // 成功后的回调函数，发送注销信息
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    resp.setStatus(200);
+                    out.write("注销成功");
+                    out.flush();
+                    out.close();
+                })
+//                .logoutSuccessUrl("/index")//登录成功要跳转的页面
                 .deleteCookies()//清除cookie
                 .clearAuthentication(true)//清除认证信息
                 .invalidateHttpSession(true)//使HttpSession失效  默认就会清除
                 .permitAll()//绕开过滤器
                 .and()
+                .rememberMe()//记住我，配置这个就行
+                .key("zyx") //这个key原本是系统设置的UUID,但是服务端重启的话会变。
+                .tokenRepository(jdbcTokenRepository())// 自带的数据库模型，里面有对token表的操作
+                .and()
                 .csrf().disable()
                 .exceptionHandling()
-                .authenticationEntryPoint((req, resp, e) -> {
-                    resp.setContentType("application/json;charset=utf-8");
-                    PrintWriter out = resp.getWriter();
-                    resp.sendError(110);
-                    out.write("尚未登录，请先登录");
-                    out.flush();
-                    out.close();
-                });
+//                .authenticationEntryPoint((req, resp, e) -> {
+//                    resp.setContentType("application/json;charset=utf-8");
+//                    PrintWriter out = resp.getWriter();
+//                    resp.setStatus(403);
+//                    out.append("{\"code\":1,\"msg\":\"登录失败,请重新登录!\",\"data\":\"failed\"}");
+////                    out.write("尚未登录，请先登录");
+//                    out.flush();
+//                    out.close();
+//                });
+        ;
     }
 }
--- src/main/java/com/example/SpringSecurityDemo/web/HelloController.java
+++ src/main/java/com/example/SpringSecurityDemo/web/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.SpringSecurityDemo.web;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,21 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/admin/hello")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/user/hello")
+    public String user(){
+        return "user";
+    }
+
+
+    @GetMapping("/rememberme/hello")
+    public String rememberme(){
+        return "rememberme";
     }
 }
